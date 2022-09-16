e41ca32d41c823d7baf538161fce98c9207717e3
vishnu5c3 <113178216+vishnu5c3@users.noreply.github.com>
2022-09-16 15:07:30 +0530

    Update FCFS.C

--- OSLAB/EXP1/FCFS/FCFS.C
+++ OSLAB/EXP1/FCFS/FCFS.C
@@ -1,7 +1,7 @@
-//B.Vishnu//
-//204g1a05c3//
-//Name of experiment:First Come First Serve//
-//Schedule:16.09.2022//
+/*B.Vishnu
+204g1a05c3
+Name of experiment:First Come First Serve//
+Schedule:16.09.2022*/
 #include <stdio.h>
     int main(){
 	printf("Hello World\n");
